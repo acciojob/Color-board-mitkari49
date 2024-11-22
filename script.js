06c46e6abd7782ffba7c1c4ba888ac74c98396b7
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-11-22 10:13:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.addEventListener(DOMContentLoaded, functon(){
+	const squares = document .querySelectorAll('.square');
+	squares.forEach(square => {
+		square.addEventListener(mouseover, function()
+								{
+									square.style.backgroundColor = '#3498db';
+								});
+		square.addEventListener(mouseout, function(){
+			setTimeout(()=>{
+				squares.style.backgroundColor = '#eee';
+			},1000);
+		});
+	});
+});
+	
